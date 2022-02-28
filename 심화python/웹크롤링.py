789033dafbf96be162d74c6e65c970f1aaf21403
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-02-28 19:41:59 +0900

    .gitignore

--- 심화python/웹크롤링.py
+++ 심화python/웹크롤링.py
@@ -1,1 +1,23 @@
-import 
+import requests
+
+url = "http://www.naver.com"
+response = requests.get(url)
+
+print(response.text)
+
+print(response.url)
+
+print(response.content)
+
+print(response.encoding)
+
+print(response.headers)
+
+print(response.json)
+
+print(response.links)
+
+print(response.ok)
+
+print(response.status_code)
+
